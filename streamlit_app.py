798d663e3cf9c9b0208de5cff3c59d5d56016458
jcsnow83 <128848455+jcsnow83@users.noreply.github.com>
2023-03-29 11:49:22 +0100

    Update streamlit_app.py
    added close connections to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,7 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   # call function
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
   
@@ -78,6 +79,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
   
 
