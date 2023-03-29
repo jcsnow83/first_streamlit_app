0a01372c13b0863c00b1e238a8ea451bcd585203
jcsnow83 <128848455+jcsnow83@users.noreply.github.com>
2023-03-29 10:16:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?',' jackfr
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #NOT CORRECT
-my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
+my_cur.execute("INSERT INTO fruit_load_list values (add_my_fruit)")
 
 
 
